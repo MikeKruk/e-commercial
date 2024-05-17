a3d53e8092e759720f85b76aa5d51b08b6466d6a
Sergey <sk85post@gmail.com>
2024-05-17 22:10:11 +0300

    init: RSSECOMM2-43-Integration-with-CommerceTools-for-registration

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,24 @@
 export const ADD_NAME = 'ADD_NAME';
 
-export interface UserState {
+export interface IUserState {
   userName: string;
   userSurname: string;
 }
 
 // payload не обязательный
-export interface UserAction {
+export interface IUserAction {
   type: string;
   payload: string;
+}
+
+export interface ISignUpData {
+  email: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  birthdate: string;
+  street: string;
+  city: string;
+  postal_code: string;
+  country: string;
 }
