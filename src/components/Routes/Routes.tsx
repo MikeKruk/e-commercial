c211c1306a08418a35ad0588a5f2504207155be1
Sergey <148476160+sk85web@users.noreply.github.com>
2024-05-19 19:54:28 +0300

    Rssecomm2 61 implement a 404 not found page (#30)
    * feat: RSSECOMM2-61-Implement-a-404-Not-Found-Page
    
    * feat: add button to back home

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import SignIn from '../SignIn/SignIn';
+
 import ROUTES from '../../utils/routes';
+import SignIn from '../SignIn/SignIn';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import MainPage from '../MainPage/MainPage';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 const AppRoutes = () => (
   <Routes>
     <Route path={ROUTES.SIGNUP} element={<SignUpForm />} />
     <Route path={ROUTES.SIGNIN} element={<SignIn />} />
     <Route path={ROUTES.MAIN_PAGE} element={<MainPage />} />
+    <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
   </Routes>
 );
 
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,6 +2,7 @@ const ROUTES = {
   MAIN_PAGE: '/',
   SIGNIN: '/sign-in',
   SIGNUP: '/sign-up',
+  NOT_FOUND: '/not-found-404',
 };
 
 export default ROUTES;
